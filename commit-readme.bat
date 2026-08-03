be31e2e1a5d60c6d999bcad372287a904be443fc
AB <64829477+AB-invisible@users.noreply.github.com>
2026-08-03 16:02:34 +0100

    Delete commit-readme.bat

--- commit-readme.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -NoExit -ExecutionPolicy Bypass -Command "cd '%~dp0'; git add README.md; git commit -m 'Add README'; git push origin main; Write-Host 'Done.' -ForegroundColor Green"
